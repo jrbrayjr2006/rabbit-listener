cd538cfd1c6ec6ae3c401d61772d17666608151e
jbrayjr2006 <jbrayjr2006@gmail.com>
2018-05-07 14:33:50 -0500

    first commit

---
+++ src/main/java/com/jrbrayjr/rabbbit/listener/rabbitlistener/RabbitListenerApplication.java
@@ -0,0 +1,12 @@
+package com.jrbrayjr.rabbbit.listener.rabbitlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitListenerApplication.class, args);
+	}
+}
