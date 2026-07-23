90a5adcca8aea8ea372f32a145ba517301a030e4
James Bray <jrbrayjr@gmail.com>
2026-07-22 22:35:38 -0500

    update basic unit test

--- src/test/java/com/jrbrayjr/rabbbit/listener/rabbitlistener/RabbitListenerApplicationTests.java
+++ src/test/java/com/jrbrayjr/rabbbit/listener/rabbitlistener/RabbitListenerApplicationTests.java
@@ -1,16 +1,14 @@
 package com.jrbrayjr.rabbbit.listener.rabbitlistener;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class RabbitListenerApplicationTests {
+class RabbitlistenerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
 
 }
